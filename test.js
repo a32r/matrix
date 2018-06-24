0983ed95acc91abb1a8fa3892df4ef67d7fc3cb9
Admin <site@mail.com>
2018-06-24 22:53:36 +0300

    Put center item in result array

--- test.js
+++ test.js
@@ -1,12 +1,24 @@
-n = 2
+n = 3
 m = n*2-1
+
+// making matrix
+nums = []
 for (i = 0; i < m; i++) {
+	nums[i] = []
 	var row = document.createElement('tr')
 	row.id = "tr-"+i
 	document.getElementById('test').appendChild(row)
 	for (j = 0; j < m; j++) {
 		var cell = document.createElement('td')
-		cell.innerHTML = j
+		num = Math.floor(Math.random() * 1001)
+		nums[i][j] = num
+		cell.innerHTML = num
 		document.getElementById('tr-'+i).appendChild(cell)
 	}
 }
+
+// making result
+center = Math.floor(m/2)
+result = []
+result.push(nums[center][center])
+document.getElementById('result').innerHTML = result
