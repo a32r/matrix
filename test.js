3f0f11baa421d073d1386b0ec3b1abeb47ca8c88
Admin <site@mail.com>
2018-06-24 14:48:12 +0300

    Table layout

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-n = 3
-document.write(Math.pow(2*n-1,2))
-for (i = 0; i < Math.pow(2*n-1,2); i++) {
-	var cell = document.createElement('div')
-	cell.innerHTML = i
-	document.getElementById('test').appendChild(cell)
+n = 2
+m = n*2-1
+for (i = 0; i < m; i++) {
+	var row = document.createElement('tr')
+	row.id = "tr-"+i
+	document.getElementById('test').appendChild(row)
+	for (j = 0; j < m; j++) {
+		var cell = document.createElement('td')
+		cell.innerHTML = j
+		document.getElementById('tr-'+i).appendChild(cell)
+	}
 }
